5ecb1a9e6483007d2051d2be707b838ee53d7829
Andrea <andrea.vallejo@endava.com>
2020-09-08 10:35:56 -0500

    is a palindrome exercise done

--- src/main/java/ReadAnInputFile.java
+++ src/main/java/ReadAnInputFile.java
@@ -1,2 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadAnInputFile {
+
+
+    public List<String> readFile (){
+
+        List<String> textLines = new ArrayList<>();
+
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("C:\\Users\\avallejo\\OneDrive - ENDAVA\\Documents\\Repositories\\algorithmsQuiz_project_java\\src\\main\\resources\\inputFile.txt"));
+            String line = bufReader.readLine();
+            while (line !=null){
+
+                textLines.add(line);
+                line = bufReader.readLine();
+
+            }
+            bufReader.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Unable to find input File");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Input/Output issue presented");
+        }
+
+        return textLines;
+    }
+
+
+
 }
--- src/main/java/WriteAnOutputFile.java
+++ src/main/java/WriteAnOutputFile.java
@@ -1,2 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class WriteAnOutputFile {
+
+
+    public void storeOnOutputFile (List<String> palindromes) {
+
+        try {
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("C:\\Users\\avallejo\\OneDrive - ENDAVA\\Documents\\Repositories\\algorithmsQuiz_project_java\\src\\main\\resources\\outputFile.txt"));
+
+            for(int i=0; i < palindromes.size(); i++){
+                bufWriter.write(palindromes.get(i));
+                bufWriter.newLine();
+            }
+            bufWriter.flush();
+            bufWriter.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Input/Output issue presented");
+        }
+
+    }
+
+
 }
